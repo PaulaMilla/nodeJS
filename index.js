caab95845518d0a7e15cff6361ca73e4470685d8
Patricia-gl <patricia.gonzalez2101@alumnos.ubibio.cl>
2025-10-02 11:55:02 -0300

    Configuración base de datos y PUT reviews por ID

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const reviewRoutes = require('./routes/reviews.routes');
 //Puerto de escucha
 const port = 3000;
+
+//middleware para parsear JSON
+app.use(express.json());
+
+//Ruta PUT Review by ID
+app.use('/reviews', reviewRoutes);
 
 //Inicialización del server
 app.listen(port, ()=>{

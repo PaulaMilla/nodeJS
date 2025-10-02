103b7943ad60a498892cdb0f02825d8d76d739ee
Patricia-gl <patricia.gonzalez2101@alumnos.ubibio.cl>
2025-10-02 12:46:28 -0300

    PATCH contraseña usuario + POST registrar usuario

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const reviewRoutes = require('./routes/reviews.routes');
+const usuarioRoutes = require('./routes/users.routes');
 //Puerto de escucha
 const port = 3000;
 
@@ -9,6 +10,10 @@ app.use(express.json());
 
 //Ruta PUT Review by ID
 app.use('/reviews', reviewRoutes);
+//Ruta PATCH contraseña usuario
+app.use('/usuarios', usuarioRoutes);
+//Ruta POST registrar usuario
+
 
 //Inicialización del server
 app.listen(port, ()=>{

e7480e57c83f12098d7ccde691021d1d4c914945
SamuelPerez21 <samuel.perez2101@alumnos.ubiobio.cl>
2025-12-02 18:44:30 -0300

    agrega funcionalidad para actualizar comentarios de reseñas y alias de usuario

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createReview, updateReview, getAllReviews, getReviewById, deleteReview } = require('../controllers/reviews.controller');
+const { createReview, updateReview, getAllReviews, getReviewById, deleteReview, updateReviewComment } = require('../controllers/reviews.controller');
 
 // POST /reviews - Crear una nueva reseña
 router.post('/', createReview);
 
 // PUT: Actualizar una review por ID
 router.put('/:id', updateReview);
+
+// PATCH: Actualizar solo el comentario de una review
+router.patch('/:id/comentario', updateReviewComment);
 
 // GET /reviews/:id - Get review por id
 router.get('/:id', getReviewById);
